d1b05b952b068fbe25cdc4a37715bce10e4fb7c4
jhowzluk <jhonatanluksik@gmail.com>
2023-12-05 19:56:13 -0300

    Alterando CRUD Episodios

--- senaiplus/php/salvaEpisodio.php
+++ senaiplus/php/salvaEpisodio.php
@@ -29,33 +29,33 @@
 
         //insert na tabela episodios
         $sql = "INSERT "
-            ."INTO episodios (idEpisodio, nome, sinopse, lancamento," 
-            ."hora, minuto, estudio, diretor, elenco, classificacao_idClassificacao) "
+            ."INTO episodios (idEpisodio, nome, temporada, nrEpisodios, " 
+            ."sinopse, lancamento, series_idSerie, hora, minutos, diretor, elenco) "
             ."VALUES (".$id.", "
             ."'".$nomeEpisodio."', "
-            ."'".$sinopseEpisodio."',"
-            ."'".$lancamentoEpisodio."',"
+            ."'".$nrTemporada."', "
+            ."'".$nrEpisodio."', "
+            ."'".$sinopseEpisodio."', "
+            ."'".$lancamentoEpisodio."', "
+            ."'".$serieEpisodio."', "
             ."'".$horaEpisodio."', "
             ."'".$minutoEpisodio."', "
-            ."'".$estudioEpisodio."',"
-            ."'".$diretorEpisodio."',"
-            ."'".$elencoEpisodio."', "
-            ."'".$classificacaoEpisodio."');";
-
+            ."'".$diretorEpisodio."', "
+            ."'".$elencoEpisodio."'); ";
     
         $result = mysqli_query($conn,$sql);    
 
     } else {
-
+    
         //update na tabela episodios 
         $sql = "UPDATE episodios "
-            ."SET nome='".$nomeEpisodio."', sinopse='".$sinopseEpisodio."', lancamento='".$lancamentoEpisodio."'," 
-            ."hora='".$horaEpisodio."', minuto='".$minutoEpisodio."', estudio='".$estudioEpisodio."', diretor='".$diretorEpisodio."',"
-            ."elenco='".$elencoEpisodio."', classificacao_idClassificacao='".$classificacaoEpisodio."' "
+            ."SET nome='".$nomeEpisodio."', temporada='".$nrTemporada."', nrEpisodios='".$nrEpisodio."'," 
+            ."sinopse='".$sinopseEpisodio."', lancamento='".$lancamentoEpisodio."', series_idSerie='".$serieEpisodio."', hora='".$horaEpisodio."',"
+            ."minutos='".$minutoEpisodio."', diretor='".$diretorEpisodio."', elenco='".$elencoEpisodio."' "
             ."WHERE idEpisodio='".$idEpisodio."';";
 
         $result = mysqli_query($conn,$sql);  
-    
+        
 
     }
 
@@ -75,94 +75,24 @@
         $novoNome = md5($_FILES['nVideoEpisodio']['name']).'.'.$extensao;        
         
         //Verificar se o diretório existe, ou criar a pasta
-        if(is_dir('../video/filme/'.$id.'/')){
+        if(is_dir('../video/serie/'.$serieEpisodio.'/'.$nrEpisodio.'/')){
             //Existe
-            $diretorio = '../video/filme/'.$id.'/';
+            $diretorio = '../video/serie/'.$serieEpisodio.'/'.$nrEpisodio.'/';
         }else{
             //Criar pq não existe
-            $diretorio = mkdir('../video/filme/'.$id.'/');
+            $diretorio = mkdir('../video/serie/'.$serieEpisodio.'/'.$nrEpisodio.'/');
         }
 
         //Cria uma cópia do arquivo local na pasta do projeto
-        move_uploaded_file($_FILES['nVideoEpisodio']['tmp_name'], '../video/filme/'.$id.'/'.$novoNome);
+        move_uploaded_file($_FILES['nVideoEpisodio']['tmp_name'], '../video/serie/'.$serieEpisodio.'/'.$nrEpisodio.'/'.$novoNome);
 
         //Caminho que será salvo no banco de dados
-        $dirVideo = 'video/filme/'.$id.'/'.$novoNome;
+        $dirVideo = 'video/serie/'.$serieEpisodio.'/'.$nrEpisodio.'/'.$novoNome;
 
         include("conexao.php");
         //UPDATE
         $sql = "UPDATE episodios "
                 ." SET video = '$dirVideo' "
-                ." WHERE idEpisodio = $id;";
-        $result = mysqli_query($conn,$sql);
-        mysqli_close($conn);
-    }
-
-     //imagem capa 
-     if($_FILES['nCapaEpisodio']['tmp_name'] != ""){
-
-        //Usar o mesmo nome do arquivo original
-        //$nomeArq = $_FILES['Foto']['name'];
-        //...
-        //OU
-        //Pega a extensão do arquivo e cria um novo nome pra ele (MD5 do nome original)
-        $extensao = pathinfo($_FILES['nCapaEpisodio']['name'], PATHINFO_EXTENSION);
-        $novoNome = md5($_FILES['nCapaEpisodio']['name']).'.'.$extensao;        
-        
-        //Verificar se o diretório existe, ou criar a pasta
-        if(is_dir('../image/filme/capa/'.$id.'/')){
-            //Existe
-            $diretorio = '../image/filme/capa/'.$id.'/';
-        }else{
-            //Criar pq não existe
-            $diretorio = mkdir('../image/filme/capa/'.$id.'/');
-        }
-
-        //Cria uma cópia do arquivo local na pasta do projeto
-        move_uploaded_file($_FILES['nCapaEpisodio']['tmp_name'], '../image/filme/capa/'.$id.'/'.$novoNome);
-
-        //Caminho que será salvo no banco de dados
-        $dirImagem = 'image/filme/capa/'.$id.'/'.$novoNome;
-
-        include("conexao.php");
-        //UPDATE
-        $sql = "UPDATE episodios "
-                ." SET capa = '$dirImagem' "
-                ." WHERE idEpisodio = $id;";
-        $result = mysqli_query($conn,$sql);
-        mysqli_close($conn);
-    }
-
-     //imagem carousel
-     if($_FILES['nCarouselEpisodio']['tmp_name'] != ""){
-
-        //Usar o mesmo nome do arquivo original
-        //$nomeArq = $_FILES['Foto']['name'];
-        //...
-        //OU
-        //Pega a extensão do arquivo e cria um novo nome pra ele (MD5 do nome original)
-        $extensao = pathinfo($_FILES['nCarouselEpisodio']['name'], PATHINFO_EXTENSION);
-        $novoNome = md5($_FILES['nCarouselEpisodio']['name']).'.'.$extensao;        
-        
-        //Verificar se o diretório existe, ou criar a pasta
-        if(is_dir('../image/filme/carousel/'.$id.'/')){
-            //Existe
-            $diretorio = '../image/filme/carousel/'.$id.'/';
-        }else{
-            //Criar pq não existe
-            $diretorio = mkdir('../image/filme/carousel/'.$id.'/');
-        }
-
-        //Cria uma cópia do arquivo local na pasta do projeto
-        move_uploaded_file($_FILES['nCarouselEpisodio']['tmp_name'], '../image/filme/carousel/'.$id.'/'.$novoNome);
-
-        //Caminho que será salvo no banco de dados
-        $dirImagem = 'image/filme/carousel/'.$id.'/'.$novoNome;
-
-        include("conexao.php");
-        //UPDATE
-        $sql = "UPDATE episodios "
-                ." SET carouselimage = '$dirImagem' "
                 ." WHERE idEpisodio = $id;";
         $result = mysqli_query($conn,$sql);
         mysqli_close($conn);
